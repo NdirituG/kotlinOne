d037a44c22fd93c0a5ba07318cb487d0671e73b7
NdirituG <denisndiritug@gmail.com>
2022-06-04 19:11:09 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Hello World!")
+    println("this is my first Kotlin code!! am so happy!!!")
+    //fun is a Kotlin keyword showing that a function has been written.
+    //var or val used to declare variables. var can be changed while val cannot
+    var name = "Denis"
+    val birthDate = "2001"
+    println("My name is $name and was born in the year $birthDate.")
+
+    //variables can also be written as such to specify datatypes
+    var school:String = "Gretsa"
+    var age:Int = 21
+    println("I am at $school university at the age of $age")
+
+    var one = "Duncan"
+    var two = "Ndiritu"
+    println("$one $two")
+}
