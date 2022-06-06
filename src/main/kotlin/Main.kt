375bfcd8018bb8e07a17c9570007ba794a77f6e3
NdirituG <denisndiritug@gmail.com>
2022-06-06 08:39:00 +0300

    update to the Main.kt test file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,10 +9,56 @@ fun main() {
 
     //variables can also be written as such to specify datatypes
     var school:String = "Gretsa"
-    var age:Int = 21
-    println("I am at $school university at the age of $age")
+    var age1:Int = 21
+    println("I am at $school university at the age of $age1")
 
     var one = "Duncan"
     var two = "Ndiritu"
     println("$one $two")
-}
+
+    val border = "'-._,-'"
+    val timesToRepeat = 4
+    fun createBorder(border: String,timesToRepeat: Int){
+        repeat(timesToRepeat){
+            print(border)
+        }
+        println("")
+    }
+    createBorder(border, timesToRepeat)
+    println("Happy Birthday, Jhansi!")
+    createBorder(border, timesToRepeat)
+
+    var age = 24
+    var layers = 5
+    fun printCakeTop(age:Int){
+        repeat(age + 2){
+            print("=")
+        }
+        println()
+    }
+    fun printCakeCandles(age:Int){
+        print(" ")
+        repeat(age){
+            print(",")
+        }
+        println()//print an empty line
+
+        print(" ")
+        repeat(age){
+            print("|")
+        }
+        println()
+    }
+
+    fun printCakeBottom(age:Int, layers:Int){
+        repeat(layers){
+            repeat(age + 2){
+                print("@")
+            }
+            println()
+        }
+    }
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+    }
